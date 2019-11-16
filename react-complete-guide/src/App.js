9d6675858ebae858d9a6026f3e5b7818ecd14c74
Pat Palmerston <patpalmerston@gmail.com>
2019-11-15 22:33:24 -0800

    person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
-
-import "./App.css";
+import React, { Component } from 'react';
+import Person from './person/Person';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <h1>Hi I'm a React App</h1>
+                <Person />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ react-complete-guide/src/person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <div className='person'>Person</div>;
+};
+
+export default person;
