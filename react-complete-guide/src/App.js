1a68cb4fc0dbe5494aa6c36484ddf2c84d09908a
Pat Palmerston <patpalmerston@gmail.com>
2019-11-15 23:03:07 -0800

    passing state with class component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,16 +3,33 @@ import Person from './person/Person';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            persons: [
+                { name: 'Max', age: 29, hobbies: 'Racing' },
+                { name: 'Manu', age: 28, hobbies: 'lifting' },
+                { name: 'Steph', age: 26, hobbies: 'growing' }
+            ]
+        };
+    }
     render() {
+        console.log(this.state);
         return (
             <div className='App'>
                 <h1>Hi I'm a React App</h1>
-                <Person name='Mac' age='28' />
-                <Person name='mike' age='18'>
-                    {' '}
-                    Hobbies: Racing
-                </Person>
-                <Person name='jill' age='38' />
+                <button>Switch Name</button>
+
+                {this.state.persons.map((x, i) => (
+                    <div key={i}>
+                        {
+                            <Person name={x.name} age={x.age}>
+                                {' '}
+                                <h6>Hobbies: {x.hobbies}</h6>
+                            </Person>
+                        }
+                    </div>
+                ))}
             </div>
         );
     }
--- react-complete-guide/src/person/Person.js
+++ react-complete-guide/src/person/Person.js
@@ -3,7 +3,10 @@ import React from 'react';
 const person = props => {
     return (
         <div className='person'>
-            I'm {props.name} and I am {props.age} years old
+            <p>
+                I'm {props.name} and I am {props.age} years old
+            </p>
+            <div>{props.children}</div>
         </div>
     );
 };
