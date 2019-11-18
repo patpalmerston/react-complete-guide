4a89715c3f7ee5bc9b87ee9a01047acd67423e30
Pat Palmerston <patpalmerston@gmail.com>
2019-11-18 15:37:54 -0800

    usestate and boolean values

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,48 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './person/Person';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            persons: [
-                { name: 'Max', age: 29, hobbies: 'Racing' },
-                { name: 'Manu', age: 28, hobbies: 'lifting' },
-                { name: 'Steph', age: 26, hobbies: 'growing' }
-            ]
-        };
-    }
+const App = () => {
+    const [personState, setPersonState] = useState({
+        persons: [
+            { name: 'Max', age: 29, hobbies: 'Racing' },
+            { name: 'Manu', age: 28, hobbies: 'lifting' },
+            { name: 'Steph', age: 26, hobbies: 'growing' }
+        ]
+    });
 
-    switchNameHandler = () => {
-        this.setState({
-            persons: [
-                { name: 'Larry', age: 29, hobbies: 'making' },
-                { name: 'scary', age: 28, hobbies: 'shit' },
-                { name: 'mary', age: 26, hobbies: 'happen' }
-            ]
-        });
+    const [show, setShow] = useState(true);
+
+    const switchNameHandler = props => {
+        setShow(!show);
+        {
+            !show
+                ? setPersonState({
+                      persons: [
+                          { name: 'Larry', age: 29, hobbies: 'making' },
+                          { name: 'scary', age: 28, hobbies: 'shit' },
+                          { name: 'mary', age: 26, hobbies: 'happen' }
+                      ]
+                  })
+                : setPersonState({
+                      persons: [
+                          { name: 'Max', age: 29, hobbies: 'Racing' },
+                          { name: 'Manu', age: 28, hobbies: 'lifting' },
+                          { name: 'Steph', age: 26, hobbies: 'growing' }
+                      ]
+                  });
+        }
     };
 
-    render() {
-        console.log(this.state);
-        return (
-            <div className='App'>
-                <h1>Hi I'm a React App</h1>
-                <button onClick={this.switchNameHandler}> SwitchName</button>
-                {this.state.persons.map((x, i) => (
-                    <div key={i}>
-                        {
-                            <Person name={x.name} age={x.age}>
-                                {' '}
-                                <h6>Hobbies: {x.hobbies}</h6>
-                            </Person>
-                        }
-                    </div>
-                ))}
-            </div>
-        );
-    }
-}
+    return (
+        <div className='App'>
+            <h1>Hi I'm a React App</h1>
+            <button onClick={switchNameHandler}> SwitchName</button>
+            {personState.persons.map((x, i) => (
+                <div key={i}>
+                    {
+                        <Person name={x.name} age={x.age}>
+                            {' '}
+                            <h6>Hobbies: {x.hobbies}</h6>
+                        </Person>
+                    }
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default App;
+
+// constructor() {
+//     super();
+//     this.state = {
+//         persons: [
+//             { name: 'Max', age: 29, hobbies: 'Racing' },
+//             { name: 'Manu', age: 28, hobbies: 'lifting' },
+//             { name: 'Steph', age: 26, hobbies: 'growing' }
+//         ]
+//     };
+// }
