0d783f7a211be4299b758e940a1ffb13688793f1
Pat Palmerston <patpalmerston@gmail.com>
2020-04-07 14:38:28 -0700

    update before life cycle clean upj

--- react-complete-guide/src/components/persons/persons.js
+++ react-complete-guide/src/components/persons/persons.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Person from './person/Person';
 
 class Persons extends Component {
-    static getDerivedStateFromProps(props, state) {
-        console.log('[Persons.js] get DerivedStateFromProps');
-        return state;
-    }
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] get DerivedStateFromProps');
+    //     return state;
+    // }
+
+    // componentWillReceiveProps(props) {
+    //     console.log('[Persons.js] componentWillRecieveProps', props)
+    // }
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('[Persons.js] shouldComponentUpdate');
@@ -16,7 +20,12 @@ class Persons extends Component {
         console.log('[Persons.js] getSnapshotBeforeUpdate');
     }
 
-    componentDidMount() {}
+    // componentWillUpdate() {}
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
 
     render() {
         return this.props.persons.map((x, i) => {
