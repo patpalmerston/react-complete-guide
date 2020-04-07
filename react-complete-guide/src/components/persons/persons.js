0c20da2a428397cf7c3298c6935cfedaf1bbe74c
Pat Palmerston <patpalmerston@gmail.com>
2020-04-07 13:28:31 -0700

    life cycles

--- react-complete-guide/src/components/persons/persons.js
+++ react-complete-guide/src/components/persons/persons.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './person/Person';
 
-const Persons = props =>
-    props.persons.map((x, i) => {
-        console.log('x', x);
-        return (
-            <Person
-                click={props.clicked(i)}
-                name={x.name}
-                age={x.age}
-                key={x.id}
-                changed={e => props.changed(e, x.id)}
-            />
-        );
-    });
+class Persons extends Component {
+    static getDerivedStateFromProps(props, state) {
+        console.log('[Persons.js] get DerivedStateFromProps');
+        return state;
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+    }
+
+    componentDidMount() {}
+
+    render() {
+        return this.props.persons.map((x, i) => {
+            console.log('x', x);
+            return (
+                <Person
+                    click={() => this.props.clicked(i)}
+                    name={x.name}
+                    age={x.age}
+                    key={x.id}
+                    changed={e => this.props.changed(e, x.id)}
+                />
+            );
+        });
+    }
+}
 
 export default Persons;
