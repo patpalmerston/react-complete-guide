e4f35e118a3fb3dcfc30ad96f60c4f3aa40afded
Pat Palmerston <patpalmerston@gmail.com>
2020-04-08 13:57:44 -0700

    using the auxillary hoc to wrap component

--- react-complete-guide/src/components/persons/person/Person.js
+++ react-complete-guide/src/components/persons/person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Auxillary from '../../../hoc/Auxillary';
 
 import classes from './Person.css';
 
@@ -9,20 +10,22 @@ class Person extends Component {
     // you can render an array of multiple elements without the parent div, but each array should have a key
     render() {
         console.log('[Person.js] rendering...');
-        return [
-            // <div className={classes.Person}>
-            <p key='i1' onClick={this.props.click}>
-                I'm {this.props.name} and I am {this.props.age} years old!
-            </p>,
-            <p key='i2'>{this.props.children}</p>,
-            <input
-                key='i3'
-                type='text'
-                onChange={this.props.changed}
-                value={this.props.name}
-            />,
-            // </div>
-        ];
+        return (
+            <Auxillary>
+                {/* <div className={classes.Person}> */}
+                <p key='i1' onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!
+                </p>
+                ,<p key='i2'>{this.props.children}</p>,
+                <input
+                    key='i3'
+                    type='text'
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
+                {/* </div> */}
+            </Auxillary>
+        );
     }
 }
 
