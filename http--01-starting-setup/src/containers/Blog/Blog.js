5ee889b4740de85e971187832e8b03328b826f4c
Pat Palmerston <patpalmerston@gmail.com>
2020-06-22 15:01:04 -0700

    error handling locally

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -10,6 +10,7 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null,
+        error: false,
     };
 
     componentDidMount() {
@@ -25,6 +26,10 @@ class Blog extends Component {
                 });
                 this.setState({ posts: updatedPosts });
                 // console.log( response );
+            })
+            .catch((err) => {
+                // console.log(err);
+                this.setState({ error: true });
             });
     }
 
@@ -33,16 +38,22 @@ class Blog extends Component {
     };
 
     render() {
-        const posts = this.state.posts.map((post) => {
-            return (
-                <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            );
-        });
+        let posts = (
+            <p style={{ textAlign: 'center' }}>Something went wrong..</p>
+        );
+
+        if (!this.state.error) {
+            posts = this.state.posts.map((post) => {
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                );
+            });
+        }
 
         return (
             <div>
