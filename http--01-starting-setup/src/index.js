d41545fcf3e1325f8101477270fe170b600b315c
Pat Palmerston <patpalmerston@gmail.com>
2020-06-22 15:40:39 -0700

    adding and removing axios interceptors

--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(
+    (req) => {
+        console.log(req);
+        return req;
+    },
+    (err) => {
+        console.log(err);
+        return Promise.reject(err);
+    }
+);
+
+axios.interceptors.response.use(
+    (res) => {
+        console.log(res);
+        return res;
+    },
+    (err) => {
+        console.log(err);
+        return Promise.reject(err);
+    }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
