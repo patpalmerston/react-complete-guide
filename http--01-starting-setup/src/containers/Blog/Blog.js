a2a994299f6a9266079b73dfacb1b6a8196feb11
Pat Palmerston <patpalmerston@gmail.com>
2020-06-27 09:50:13 -0700

    addd switch to load single route

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 //Link allows us to navigate components without reloading the html page, "a" tags reload the whole page
-import { Route, NavLink } from 'react-router-dom';
+// The order of links is important
+// Switch allows us to only load one link at a time
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
@@ -41,9 +43,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path='/' exact component={Posts} />
-                <Route path='/new-post' component={NewPost} />
-                <Route path='/:id' exact component={FullPost} />
+                <Switch>
+                    <Route path='/' exact component={Posts} />
+                    <Route path='/new-post' component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
