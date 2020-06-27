92fa48113e0849e909f61ff08651f45e8abe8082
Pat Palmerston <patpalmerston@gmail.com>
2020-06-27 09:17:45 -0700

    active route styling

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -15,7 +15,15 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li>
-                                <NavLink to='/' exact>
+                                <NavLink
+                                    to='/'
+                                    exact
+                                    activeClassName='my-active'
+                                    activeStyle={{
+                                        color: '#fa923f',
+                                        textDecoration: 'underline',
+                                    }}
+                                >
                                     Home
                                 </NavLink>
                             </li>
