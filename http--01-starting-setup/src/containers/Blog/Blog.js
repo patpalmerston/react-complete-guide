5c86ec2ecee7657c126e8a145bcc169e93ea19ab
Pat Palmerston <patpalmerston@gmail.com>
2020-07-01 13:05:34 -0700

    redirecting routes

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 //Link allows us to navigate components without reloading the html page, "a" tags reload the whole page
 // The order of links is important
 // Switch allows us to only load one link at a time
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
 // import FullPost from './FullPost/FullPost';
@@ -46,6 +46,7 @@ class Blog extends Component {
                 <Switch>
                     <Route path='/new-post' component={NewPost} />
                     <Route path='/posts' component={Posts} />
+                    <Redirect from='/' to='/posts' />
                 </Switch>
             </div>
         );
